feb1db96d9542c4ab30ab833dd4b11aba053dc61
addicted <dhookingvm@gmail.com>
2024-07-04 18:30:58 +0200

    Add files via upload

--- autosecure.js
+++ autosecure.js
@@ -1,41 +1,11 @@
-const discord = require("discord.js");
-const eventHandler = require("./Handlers/eventHandler.js");
-const { queryParams } = require("../db/db.js");
-// Starting discord client
-module.exports = async (token, username) => {
-  try {
-    let status = await queryParams(`SELECT * FROM autosecure WHERE user_id=?`, [username])
-    if (status.length != 0) {
-      status = status[0].status
-    }
+const { initializeDB, queryParams } = require("./db/db");
+const { initializeController } = require("./mainbot/controllerbot");
+const initializeBots = require("./mainbot/handlers/initializeBots");
 
-    let client
-    if (status) {
-      client = new discord.Client({
-        intents: ["Guilds"], // Adding intents  
-        presence: {
-          activities: [
-            {
-              name: "fish",
-              type: discord.ActivityType.Custom,
-              state: status.split("|")[0],
-            },
-          ],
-          status: status.split("|")[1]
-        }
-      });
-    } else {
-      client = new discord.Client({
-        intents: ["Guilds"], // Adding intents
-      });
-    }
-    client.username = username;
-    client.hits = new Map();
-    eventHandler(client, token);
-    await client.login(token);
-    return client;
-  } catch (e) {
-    console.log(`${e} | ${token}`)
-    return false
-  }
-};
+initializeController().then(() => { console.log(`Initialized Controller!`) })
+initializeDB().then(() => {
+    console.log(`Initialized DB`)
+    initializeBots().then(() => {
+        console.log(`Initialized Bots`)
+    })
+})
